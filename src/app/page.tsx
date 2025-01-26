f9138beec2145d7122bc88aad855b703650157dd
ekagra0012 <ekagraagrawal@gmail.com>
2025-01-27 02:26:48 +0530

    Initial commit of jira-clone

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="">
+      <Button size="xs">
+        Primary123
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>
+      <Button variant="teritary">
+        Teritary
+      </Button>
+
+    </div>
+  )
+}
